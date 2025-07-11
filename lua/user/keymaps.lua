5859b34c61b0751fbe38c07d506a2844eb3b2aad
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-07-11 14:55:02 +0300

    rsync keymap

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -4,3 +4,6 @@ vim.keymap.set("n", "<leader><esc>", ":nohlsearch<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>cc", ":CodeCompanionChat Toggle<CR>", { noremap = true })
 vim.keymap.set("v", "<leader>cc", ":CodeCompanion <CR>", { noremap = true })
 
+--rsync up/down file
+vim.keymap.set("n", "<leader>ru", "RsyncUpFile <CR>", {noremap = true})
+vim.keymap.set("n", "<leader>rd", "RsyncDownlFile <CR>", {noremap = true})
