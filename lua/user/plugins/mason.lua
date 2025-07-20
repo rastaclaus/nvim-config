ee6dce2daffbfb865ff3abb6f93ba97e6fa0da05
Artem Shestopalov <tema.rastaclaus2016@yandex.ru>
2025-07-20 21:54:08 +0300

    pylsp no chances

--- lua/user/plugins/mason.lua
+++ lua/user/plugins/mason.lua
@@ -17,7 +17,6 @@ return {
 			ensure_installed = {
 				"lua-language-server",
 				"ruff",
-				"pylsp",
 				"stylua",
 				"json-lsp",
 				"jq",
@@ -27,29 +26,29 @@ return {
 			},
 		})
 	end,
-	init = function(_)
-		local pylsp = require("mason-registry").get_package("python-lsp-server")
-		pylsp:on("install:success", function()
-			local function mason_package_path(package)
-				local path = vim.fn.resolve(vim.fn.stdpath("data") .. "/mason/packages/" .. package)
-				return path
-			end
-
-			local path = mason_package_path("python-lsp-server")
-			local command = path .. "/venv/bin/pip"
-			local args = {
-				"install",
-				"-U",
-				"pylsp-mypy",
-			}
-
-			require("plenary.job")
-				:new({
-					command = command,
-					args = args,
-					cwd = path,
-				})
-				:start()
-		end)
-	end,
+	-- init = function(_)
+	-- 	local pylsp = require("mason-registry").get_package("python-lsp-server")
+	-- 	pylsp:on("install:success", function()
+	-- 		local function mason_package_path(package)
+	-- 			local path = vim.fn.resolve(vim.fn.stdpath("data") .. "/mason/packages/" .. package)
+	-- 			return path
+	-- 		end
+	--
+	-- 		local path = mason_package_path("python-lsp-server")
+	-- 		local command = path .. "/venv/bin/pip"
+	-- 		local args = {
+	-- 			"install",
+	-- 			"-U",
+	-- 			"pylsp-mypy",
+	-- 		}
+	--
+	-- 		require("plenary.job")
+	-- 			:new({
+	-- 				command = command,
+	-- 				args = args,
+	-- 				cwd = path,
+	-- 			})
+	-- 			:start()
+	-- 	end)
+	-- end,
 }
